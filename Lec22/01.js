93dd012ce201367f4da6f41071a71e35379b9652
Rachit Garg <rachitgarg1208@gmail.com>
2024-11-01 12:36:26 +0530

    Functions

---
+++ Lec22/01.js
@@ -0,0 +1,12 @@
+// function declaration 
+
+function addOne(num) {
+    return num + 1
+}
+console.log(addOne(5));
+
+const addTwo = function (value) {
+    return value + 2
+}
+console.log(addTwo(5));
+
