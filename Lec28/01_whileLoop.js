2af12573337e33d497080e1f2151d0035a076534
Rachit Garg <rachitgarg1208@gmail.com>
2024-11-01 20:35:20 +0530

    While

---
+++ Lec28/01_whileLoop.js
@@ -0,0 +1,16 @@
+// let a, b
+// a = 10
+// b = 0
+
+// while (b < a) {
+//     console.log(b);
+//     b++
+// }
+
+let myArr = ["Hi", "Hello", "Bye"]
+let arr = 0
+
+while (arr < myArr.length) {
+    console.log(myArr[arr])
+    arr++
+}
---
+++ Lec28/02_doWhileLoop.js
@@ -0,0 +1,16 @@
+let score = 11
+
+// do {
+//     console.log(score);
+
+//     score++
+// } while (score <= 10);
+
+do {
+    console.log(score);
+
+    score++
+} while (score <= 10);
+
+// do while loop is a loop where a statement is executed at least one time
+// because the do is done first and then the while condition is checked 
